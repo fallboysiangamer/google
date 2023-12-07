fb5cc6c01e34bf1e572b1c7233b9dfdc31f41f3b
fallboysiangamer <153223301+fallboysiangamer@users.noreply.github.com>
2023-12-07 10:54:03 -0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://github.com/";//your website url
+  let target = "https://google.com/";//fallboysiangaymergoogle.com
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
